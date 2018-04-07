fbd5dbec26ac2e542afb17bb41ed3f21d5978df2
Raffaele Marcello <marcelloraffaele@gmail.com>
2018-04-07 14:12:32 +0200

    added dependency for jquery and bootstrap

--- src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
+++ src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Devopsbuddy2Application {
+public class DevopsbuddyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Devopsbuddy2Application.class, args);
+		SpringApplication.run(DevopsbuddyApplication.class, args);
 	}
 }
