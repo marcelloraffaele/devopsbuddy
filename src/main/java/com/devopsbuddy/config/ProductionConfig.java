4a8be297d5a278c8c9339f0802200599e0f48f69
Raffaele Marcello <marcelloraffaele@gmail.com>
2018-04-10 23:32:16 +0200

    added logic mail

---
+++ src/main/java/com/devopsbuddy/config/ProductionConfig.java
@@ -0,0 +1,22 @@
+package com.devopsbuddy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+
+import com.devopsbuddy.backend.service.EmailService;
+import com.devopsbuddy.backend.service.MockEmailService;
+import com.devopsbuddy.backend.service.SmtpEmailService;
+
+@Configuration
+@Profile("prod")
+@PropertySource("classpath:application-prod.properties")
+public class ProductionConfig {
+
+	@Bean
+	public EmailService getEmailService() {
+		return new SmtpEmailService();
+	}
+	
+}
--- src/main/java/com/devopsbuddy/web/controllers/ContactController.java
+++ src/main/java/com/devopsbuddy/web/controllers/ContactController.java
@@ -2,12 +2,14 @@ package com.devopsbuddy.web.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.devopsbuddy.backend.service.EmailService;
 import com.devopsbuddy.web.domain.frontend.FeedbackPojo;
 
 @Controller
@@ -17,6 +19,9 @@ public class ContactController {
 	
 	public static final String FEEDBACK_MODEL_KEY="feedback";
 	public static final String CONTACT_US_VIEW_NAME="contact/contact";
+	
+	@Autowired
+	private EmailService emailService;
 	
 	@RequestMapping(value ="/contact", method=RequestMethod.GET)
 	public String contactGet( ModelMap model ) {
@@ -28,6 +33,7 @@ public class ContactController {
 	@RequestMapping(value ="/contact", method=RequestMethod.POST)
 	public String contactPost( @ModelAttribute(FEEDBACK_MODEL_KEY) FeedbackPojo feedback  ) {
 		LOG.debug("feedback pojo={}", feedback);
+		emailService.sendFeedbackEmail(feedback);
 		return CONTACT_US_VIEW_NAME;
 	}
 	
